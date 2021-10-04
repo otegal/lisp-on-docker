13b08e79655445136db097bfbcbddd9e95c16edd
hhiroki <gyouzakuuka@gmail.com>
2021-10-05 00:06:21 +0900

    add describe-path

--- src/wizard-adventure.lisp
+++ src/wizard-adventure.lisp
@@ -2,9 +2,20 @@
 			(garden (you are in a beautiful garden. there is a well in front of you.))
 			(attic (you are in the attic. there is a giant welding touch in the corner.))))
 
+(defparameter *edges* '((living-room (garden west door)
+				     (attic upstairs ladder))
+			(garder (living-room east door))
+			(attic (living-room downstairs ladder))))
+
+
 (defun describe-location (location nodes)
+  ;; cadr = cdr + car
   (cadr (assoc location nodes)))
 
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
 ;; Test exec
- (print (describe-location 'living-room *nodes*))
+(print (describe-location 'living-room *nodes*))
+(print (describe-path '(garden west door)))
 
